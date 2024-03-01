0579b162bb8799adf179c945b5d0c51b578ebd4e
Juniperberries112 <147074069+Juniperberries112@users.noreply.github.com>
2024-03-01 10:32:50 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,30 @@
+const myImage = document.querySelector("img");
+
+myImage.onclick = () => {
+  const mySrc = myImage.getAttribute("src");
+  if (mySrc === "images/parakeet.png") {
+    myImage.setAttribute("src", "images/parakeet2.png");
+  } else {
+    myImage.setAttribute("src", "images/parakeet.png");
+  }
+};
+
+let myButton = document.querySelector("button");
+let myHeading = document.querySelector("h1");
+
+function setUserName() {
+  const myName = prompt("Please enter your name.");
+  localStorage.setItem("name", myName);
+  myHeading.textContent = `Parakeets are cool, ${myName}`;
+}
+
+if (!localStorage.getItem("name")) {
+  setUserName();
+} else {
+  const storedName = localStorage.getItem("name");
+  myHeading.textContent = `Parakeets are cool, ${storedName}`;
+}
+
+myButton.onclick = () => {
+  setUserName();
+};
